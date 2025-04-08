35afbb841844389eabbf0d893397d159ca49249c
HydrolienF <hydrolien.f@gmail.com>
2025-04-08 19:34:16 +0200

    Start basic test Not working for now.

--- src/test/java/net/mvndicraft/bonemeallimiter/BoneMealLimiterPluginTest.java
+++ src/test/java/net/mvndicraft/bonemeallimiter/BoneMealLimiterPluginTest.java
@@ -1,14 +1,36 @@
 package net.mvndicraft.bonemeallimiter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import org.bukkit.World;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BoneMealLimiterPluginTest {
 
+    private ServerMock server;
+    private World world;
+    private BoneMealLimiterPlugin plugin;
+    private PlayerMock player;
+
+    @BeforeEach
+    public void setUp() {
+        // server = MockBukkit.mock();
+        // server.addSimpleWorld("world");
+        // world = server.getWorld("world");
+        // plugin = MockBukkit.load(BoneMealLimiterPlugin.class);
+
+        // player = server.addPlayer();
+
+        // player.setGameMode(GameMode.SURVIVAL);
+    }
+
     // example test
     @Test
-    void additionTest() {
-        int n = 3 + 3;
-        assertEquals(6, n, "3 + 3 should be 6");
+    void testPlace() {
+        // Block block = server.getWorld("world").getBlockAt(0, 0, 0);
+        // block.setType(Material.CARROTS);
+        // player.simulateBlockPlace(Material.BONE_MEAL, new Location(world, 0, 0, 0));
+        // assertEquals(Material.CARROTS, block.getType());
     }
 }
