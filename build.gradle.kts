5504f331f4a39e68cdad20d6a7d0075081ebf672
HydrolienF <hydrolien.f@gmail.com>
2026-01-29 13:56:13 +0100

    Update shadow to match gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("io.github.goooler.shadow") version "8.1.8"
+    id("com.gradleup.shadow") version "9.3.1"
     `maven-publish`
     checkstyle // Ensures correctly formatted code
     pmd // Code quality checks
